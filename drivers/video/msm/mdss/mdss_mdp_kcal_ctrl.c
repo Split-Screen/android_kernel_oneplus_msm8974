8c540d22c4da303b385f31b264ac0f65b16f0239
Lokesh Chamane <lokesh.c703@gmail.com>
2015-03-06 23:59:04 -0500

    Disable kcal by default
    Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
+++ drivers/video/msm/mdss/mdss_mdp_kcal_ctrl.c
@@ -309,7 +309,7 @@ static int __devinit kcal_ctrl_probe(struct platform_device *pdev)
 
 	lut_data->red = lut_data->green = lut_data->blue = NUM_QLUT;
 	lut_data->minimum = 35;
-	lut_data->enable = 1;
+	lut_data->enable = 0;
 	lut_data->invert = 0;
 	lut_data->sat = DEF_PA;
 	lut_data->hue = 0;
